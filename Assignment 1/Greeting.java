823227c3f440a6c98a73ed2b80e1e48f3a55daac
Yakir <yakirlebovits@temple.edu>
2019-01-13 10:46:01 -0500

    Added assigment 1

---
+++ Assignment 1/Greeting.java
@@ -0,0 +1,19 @@
+/*
+ * Author: Yakir Lebovits
+ * Contact: yakirlebovits@temple.edu
+ * Professor: Prof. Fiore
+ * Lab Instructor: Chao Han
+ * Class: CIS 1068
+ * Due: 25 January, 2019
+ *
+ * This program prints out a short message, mentioning my lab instrctor
+ *
+ */
+
+public class Greeting {
+
+    public static void main(String[] args) {
+	    System.out.println("My lab for CIS 1068 meets from 10:00 to 11:50 on Mondays");
+	    System.out.println("and is taught by Chao Han");
+    }
+}
